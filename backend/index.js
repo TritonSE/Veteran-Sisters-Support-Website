5e3154f429ee556673ac566495353dd01396288d
anthony-pertsel <76824702+Anthonyp0329@users.noreply.github.com>
2025-01-07 19:47:41 -0800

    linting + git hooks

---
+++ backend/index.js
@@ -0,0 +1,62 @@
+/* eslint-disable */
+
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+
+// import { CustomError, InternalError } from "./errors.js";
+
+dotenv.config();
+
+const mongoString = process.env.DATABASE_URL;
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on("error", (error) => {
+  console.log(error);
+});
+
+database.once("connected", () => {
+  console.log("Database Connected");
+});
+
+const errorHandler = (err, req, res, next) => {
+  console.log(err);
+  if (!err) return;
+  if (!(err instanceof CustomError)) {
+    // All unhandled errors are marked as unknown internal errors
+    const e = InternalError.UNKNOWN.addContext(err.stack);
+    res.status(e.statusCode).json({
+      message: e.format(false),
+      error: true,
+    });
+  } else if (err instanceof InternalError) {
+    // Internal Error Logging
+    console.error(err.format(false));
+    res.status(err.statusCode).json({
+      message: err.format(true),
+      error: true,
+    });
+  } else {
+    res.status(err.statusCode).json({
+      message: err.format(true),
+      error: true,
+    });
+  }
+};
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// app.use("/tier", tierRouter);
+// app.use("/user", userRouter);
+// app.use("/category", categoryRouter);
+// app.use("/visualization", visRouter);
+
+app.use(errorHandler);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server Started at ${process.env.PORT}`);
+});
