cfa2efb7b4ae2d116275c1a72cb733cf823705d6
Srikar Eranky <srikar.eranky@gmail.com>
2025-01-15 15:30:51 -0800

    fixed linter issues in file routes

--- backend/controllers/file.ts
+++ backend/controllers/file.ts
@@ -1,16 +1,23 @@
 import { RequestHandler } from "express";
+
 import FileObjectModel from "../models/file";
 
+type FileUploadBody = {
+  filename: string;
+  permissions: string;
+  comments: string;
+  programs: string[];
+};
 
 export const uploadFile: RequestHandler = async (req, res, next) => {
-  const { filename, permissions, comments, programs } = req.body;
+  const { filename, permissions, comments, programs } = req.body as FileUploadBody;
 
   try {
     const fileObject = await FileObjectModel.create({
-      filename: filename,
-      permissions: permissions,
-      comments: comments,
-      programs: programs
+      filename,
+      permissions,
+      comments,
+      programs,
     });
 
     res.status(201).json(fileObject);
--- backend/models/file.ts
+++ backend/models/file.ts
@@ -2,9 +2,9 @@ import { InferSchemaType, Schema, model } from "mongoose";
 
 const fileSchema = new Schema({
   filename: { type: String, required: true },
-  permissions: { type: [String], required: true},
+  permissions: { type: [String], required: true },
   comments: { type: [String], required: true },
-  programs: { type: [String], required: true }
+  programs: { type: [String], required: true },
 });
 
 type FileObject = InferSchemaType<typeof fileSchema>;
