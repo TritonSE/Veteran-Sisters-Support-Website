e396ef8c5944ff9bd1037ea1d94bfb6e39c13eda
katelynpdn <94756838+katelynpdn@users.noreply.github.com>
2025-01-15 09:53:43 -0800

    Page height

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from "react";
+
+import styles from "./page.module.css";
+
+export default function LoginForm() {
+  return (
+    <main className={styles.page}>
+      <div className={styles.form}>
+        <div className={styles.titleBar}>
+          <div>Veteran Sisters</div>
+        </div>
+      </div>
+    </main>
+  );
+}
