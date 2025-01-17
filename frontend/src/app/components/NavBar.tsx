f13e56d4167dceb18c457765531ff2d8b56b7366
jliu0140 <122562868+jliu0140@users.noreply.github.com>
2025-01-16 16:44:06 -0800

    small adjustments to home icon

--- frontend/src/app/components/NavBar.tsx
+++ frontend/src/app/components/NavBar.tsx
@@ -2,14 +2,32 @@ import Image from "next/image";
 import Link from "next/link";
 
 import styles from "./NavBar.module.css";
-import { NavMenu } from "./NavMenu";
 
 export const NavBar = () => {
   return (
     <div className={styles.container}>
-      <NavMenu></NavMenu>
+      <div className={styles.navMenu}>
+        <div className={styles.profile}>
+          <Link href="/dummy?word=profile">
+            <div className={styles.profileFrame}>
+              <Image src="/profile_icon.svg" alt="Profile" width={24} height={24}></Image>
+            </div>
+          </Link>
+        </div>
+        <div className={styles.home}>
+          <Link href="/dummy?word=home">
+            <div className={styles.homeFrame}>
+              <div className={styles.homeBackground}>
+                <Image src="/home.svg" alt="Home" width={17.778} height={17.778}></Image>
+              </div>
+            </div>
+          </Link>
+        </div>
+      </div>
       <Link href="/dummy?word=logout">
-        <Image src="/logout.svg" alt="logout" width={20} height={20}></Image>
+        <div className={styles.logoutFrame}>
+          <Image src="/logout.svg" alt="logout" width={20} height={20}></Image>
+        </div>
       </Link>
     </div>
   );
