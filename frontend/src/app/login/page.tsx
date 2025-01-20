a2cb0c8d5f4d141d4e48dbab8fa7055884df1c90
katelynpdn <94756838+katelynpdn@users.noreply.github.com>
2025-01-20 13:44:18 -0800

    Login page update

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -14,42 +14,34 @@ export default function LoginForm() {
   return (
     <main className={styles.page}>
       <div className={styles.form}>
-        <div className={styles.titleBar}>
-          <Image
-            id="logo"
-            width={142.288}
-            height={16.66}
-            src="Logo.svg"
-            alt=""
-            style={{ objectFit: "contain" }}
-          ></Image>
-        </div>
-        <div className={styles.subtitle}>Sign in to your account</div>
+        <div className={styles.subtitle}>Log in to your account</div>
         <form className={styles.innerForm} id="contactForm">
           <label htmlFor="email">Email</label>
+          <a style={{ color: "#B80037" }}> *</a>
           <input type="text" id="email" className={styles.input} required></input>
           <label htmlFor="password">
             Password
-            <a style={{ color: "#057E6F", marginLeft: "263px", marginRight: "0px" }}>
+            <a style={{ color: "#B80037" }}> *</a>
+            <a style={{ color: "#057E6F", marginLeft: "194px", marginRight: "0px" }}>
               {" "}
               Forgot your password?
             </a>
           </label>
           <input type="password" id="password" className={styles.input} required></input>
 
-          <div className={styles.rememberMeContainer}>
+          {/* <div className={styles.rememberMeContainer}>
             <input type="checkbox" id="rememberMe" className={styles.checkbox} defaultChecked />
             <label htmlFor="rememberMe" className={styles.rememberMeLabel}>
               Remember me
             </label>
-          </div>
+          </div> */}
 
-          <Button label="Sign In" className={styles.signInButton}></Button>
+          <Button label="Continue" className={styles.signInButton}></Button>
         </form>
         <div className={styles.subtitle2}>
-          <div style={{ textAlign: "center" }}>
+          <div>
             Don&apos;t have an account?
-            <a style={{ color: "#057E6F" }}> Fill out a form.</a>
+            <a style={{ color: "#057E6F" }}> Sign up.</a>
           </div>
         </div>
       </div>
