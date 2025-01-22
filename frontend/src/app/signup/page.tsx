4a32496241c76ef0f18677b6963c9b428aa9490a
katelynpdn <94756838+katelynpdn@users.noreply.github.com>
2025-01-21 18:00:08 -0800

    Form steps 2,3

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -12,13 +12,21 @@ import ProgressBar from "@/components/ProgressBar";
 
 export default function SignUpForm() {
   const [currentPage, setCurrentPage] = useState(0);
+  const [showDropdown, setShowDropdown] = useState(false);
 
   const handleNext = () => {
-    if (currentPage < 4) setCurrentPage(currentPage + 1);
+    // Go to the next form step
+    if (currentPage < 3) setCurrentPage(currentPage + 1);
   };
 
   const handlePrevious = () => {
+    // Go to the previous form step
     if (currentPage > 0) setCurrentPage(currentPage - 1);
+  };
+
+  const handleDropdown = () => {
+    // Show Address Dropdown
+    setShowDropdown((prev) => !prev);
   };
 
   const renderPage = () => {
@@ -74,6 +82,7 @@ export default function SignUpForm() {
               <form className={styles.form}>
                 <div style={{ marginBottom: "24px" }}>
                   <BackButton handlePrevious={handlePrevious} />
+                  <ProgressBar percentCompleted={30}></ProgressBar>
                 </div>
                 <div className={styles.subtitle}>Create a membership account</div>
                 <label htmlFor="email">Email</label>
@@ -103,7 +112,45 @@ export default function SignUpForm() {
                     <input type="text" id="lastName" className={styles.input}></input>
                   </div>
                 </div>
-                <div>Address Information</div>
+                <div className={styles.addressDropdown} onClick={handleDropdown}>
+                  <div style={{ fontSize: "16px", fontWeight: "600" }}>
+                    Address Information
+                    <a style={{ color: "#60696F", fontWeight: "400", fontSize: "13px" }}>
+                      {" "}
+                      Optional
+                    </a>
+                  </div>
+                  <Image
+                    id="caret"
+                    width={20}
+                    height={20}
+                    src="ic_caretdown.svg"
+                    alt=""
+                    style={{ objectFit: "contain" }}
+                  ></Image>
+                </div>
+                {showDropdown && (
+                  <div style={{ marginTop: "16px" }}>
+                    <label htmlFor="address">Street address</label>
+                    <input type="text" id="address" className={styles.input}></input>
+                    <label htmlFor="address2">Street address line 2</label>
+                    <input type="text" id="address2" className={styles.input}></input>
+                    <div className={styles.doubleInput}>
+                      <div style={{ marginRight: "16px" }}>
+                        <label htmlFor="city">City</label>
+                        <input type="text" id="city" className={styles.input} required></input>
+                      </div>
+                      <div style={{ marginRight: "16px" }}>
+                        <label htmlFor="state">State</label>
+                        <input type="text" id="state" className={styles.input}></input>
+                      </div>
+                      <div>
+                        <label htmlFor="zip">Zip code</label>
+                        <input type="text" id="zip" className={styles.input}></input>
+                      </div>
+                    </div>
+                  </div>
+                )}
                 <Button
                   label="Continue"
                   className={styles.continueButton}
@@ -120,10 +167,77 @@ export default function SignUpForm() {
           </main>
         );
       case 2:
-      // sign up 2
-      case 3:
+        // sign up 2
+        return (
+          <main className={styles.page}>
+            <div className={styles.formContainer}>
+              <div className={styles.titleBar}>
+                <Image
+                  id="logo"
+                  width={142.288}
+                  height={16.66}
+                  src="Logo.svg"
+                  alt=""
+                  style={{ objectFit: "contain" }}
+                ></Image>
+              </div>
+              <form className={styles.form}>
+                <div style={{ marginBottom: "24px" }}>
+                  <BackButton handlePrevious={handlePrevious} />
+                  <ProgressBar percentCompleted={30}></ProgressBar>
+                </div>
+                <div className={styles.subtitle}>Tell Us About Your Service</div>
+                <div style={{ marginBottom: "24px" }}>
+                  This helps us match you with the right benefits and community.
+                </div>
+                <label htmlFor="date">Date service ended</label>
+                <a style={{ color: "#B80037" }}> *</a>
+                <input
+                  type="text"
+                  id="date"
+                  className={styles.input}
+                  placeholder="MM-DD-YYYY"
+                  required
+                ></input>
+                <label htmlFor="branch">
+                  Branch of service
+                  <a style={{ color: "#B80037" }}> *</a>
+                </label>
+                <select id="branch" className={styles.input} required>
+                  <option value="">Please select</option>
+                </select>
+                <label htmlFor="status">
+                  Current military status
+                  <a style={{ color: "#B80037" }}> *</a>
+                </label>
+                <select id="status" className={styles.input} required>
+                  <option value="">Please select</option>
+                </select>
+                <label htmlFor="gender">
+                  Gender
+                  <a style={{ color: "#B80037" }}> *</a>
+                </label>
+                <select id="gender" className={styles.input} required>
+                  <option value="">Please select</option>
+                </select>
+
+                <Button
+                  label="Continue"
+                  className={styles.continueButton}
+                  onClick={handleNext}
+                ></Button>
+                <div className={styles.subtitle2}>
+                  <div style={{ textAlign: "center" }}>
+                    Already have an account?
+                    <a style={{ color: "#057E6F" }}> Log in.</a>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </main>
+        );
       // sign up 3
-      case 4:
+      case 3:
         return (
           <main className={styles.page}>
             <div className={styles.formContainer}>
