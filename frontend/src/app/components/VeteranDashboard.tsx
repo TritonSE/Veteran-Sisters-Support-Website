61dc9c68b7da6e7f7692c7f29a74dd00b2dc48af
Srikar Eranky <srikar.eranky@gmail.com>
2025-01-22 16:56:35 -0800

    veteran dashboard branch

---
+++ frontend/src/app/components/VeteranDashboard.tsx
@@ -0,0 +1,15 @@
+export type SelectStates = {
+  All: boolean;
+  BattleBuddies: boolean;
+  IAdvocacy: boolean;
+  OperationWellness: boolean;
+};
+
+export function VeteranDashboard() {
+  return (
+    <div>
+      <h1>Documents</h1>
+      <div></div>
+    </div>
+  );
+}
