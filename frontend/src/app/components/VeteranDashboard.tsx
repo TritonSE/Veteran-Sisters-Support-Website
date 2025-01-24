b08a74118308778df5c39d29ef651b7a7a17fe6e
Srikar Eranky <srikar.eranky@gmail.com>
2025-01-23 20:10:03 -0800

    implemented document select logic

--- frontend/src/app/components/VeteranDashboard.tsx
+++ frontend/src/app/components/VeteranDashboard.tsx
@@ -2,48 +2,89 @@ import { useEffect, useState } from "react";
 
 import { FileObject, getFilesByUploader } from "../api/fileApi";
 
-export type SelectStates = {
-  All: boolean;
-  BattleBuddies: boolean;
-  IAdvocacy: boolean;
-  OperationWellness: boolean;
-};
+import styles from "./VeteranDashboard.module.css";
 
 export function VeteranDashboard() {
-  const [selectStates, setSelectStates] = useState<SelectStates>({
-    All: true,
-    BattleBuddies: false,
-    IAdvocacy: false,
-    OperationWellness: false,
-  });
-
+  const [selectedProgram, setSelectedProgram] = useState<string>("All");
   const [fileObjects, setFileObjects] = useState<FileObject[]>([]);
+  const [filteredFiles, setFilteredFiles] = useState<FileObject[]>([]);
 
   useEffect(() => {
-    getFilesByUploader("Steve").then((result) => {
-      if (result.success) {
-        console.log(result.data);
-        setFileObjects(result.data);
-        console.log("fetched files");
-      } else {
-        console.log(result.error);
-      }
-    }).catch((err: unknown) => {
-      console.error(err);
-    })
+    getFilesByUploader("Steve")
+      .then((result) => {
+        if (result.success) {
+          setFileObjects(result.data);
+        } else {
+          console.log(result.error);
+        }
+      })
+      .catch((err: unknown) => {
+        console.error(err);
+      });
   }, []);
+
+  useEffect(() => {
+    if (selectedProgram === "All") {
+      setFilteredFiles(fileObjects);
+    } else {
+      setFilteredFiles(fileObjects.filter((obj) => obj.programs.includes(selectedProgram)));
+    }
+  }, [selectedProgram, fileObjects]);
 
   return (
     <div style={{ marginLeft: "100px" }}>
       <h1>Documents</h1>
-      <div>
-        {Object.keys(selectStates).map((val) => (
-          <div key={val}>{val}</div>
-        ))}
+      <div className={styles.nav}>
+        <div
+          className={styles.navItem}
+          style={{
+            backgroundColor: selectedProgram === "All" ? "rgba(5, 126, 111, 0.10)" : "white",
+          }}
+          onClick={() => {
+            setSelectedProgram("All");
+          }}
+        >
+          <p>All</p>
+        </div>
+        <div
+          className={styles.navItem}
+          style={{
+            backgroundColor:
+              selectedProgram === "BattleBuddies" ? "rgba(5, 126, 111, 0.10)" : "white",
+          }}
+          onClick={() => {
+            setSelectedProgram("BattleBuddies");
+          }}
+        >
+          <p>Battle Buddies</p>
+        </div>
+        <div
+          style={{
+            backgroundColor: selectedProgram === "IAdvocacy" ? "rgba(5, 126, 111, 0.10)" : "white",
+          }}
+          className={styles.navItem}
+          onClick={() => {
+            setSelectedProgram("IAdvocacy");
+          }}
+        >
+          <p>I Advocacy</p>
+        </div>
+        <div
+          style={{
+            backgroundColor:
+              selectedProgram === "OperationWellness" ? "rgba(5, 126, 111, 0.10)" : "white",
+          }}
+          className={styles.navItem}
+          onClick={() => {
+            setSelectedProgram("OperationWellness");
+          }}
+        >
+          <p>Operation Wellness</p>
+        </div>
       </div>
 
       <div>
-        {fileObjects.map((obj) => (
+        {filteredFiles.map((obj) => (
           <div key={obj._id}>{obj.filename}</div>
         ))}
       </div>
