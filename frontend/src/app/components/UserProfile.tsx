d42883cf0a45a999e3be12318d5cfb579cc6142d
leo3friedman <73138942+leo3friedman@users.noreply.github.com>
2025-02-12 23:15:35 -0800

    use local storage to determine the current user, use loading

--- frontend/src/app/components/UserProfile.tsx
+++ frontend/src/app/components/UserProfile.tsx
@@ -32,8 +32,8 @@ interface ProfileRenderingContext {
 }
 
 function getProfileRenderingContext(
-  viewerRole: string | undefined | null,
   viewingRole: string | undefined | null,
+  viewingId: string | undefined | null,
 ): ProfileRenderingContext {
   const context: ProfileRenderingContext = {
     invalidContext: false,
@@ -44,30 +44,34 @@ function getProfileRenderingContext(
     viewingPersonalProfile: false,
     isProgramAndRoleEditable: false,
   };
+  const viewerId = localStorage.getItem("viewerId");
+  const viewerRole = localStorage.getItem("viewerRole");
+
   const { ADMIN, STAFF, VOLUNTEER, VETERAN } = RoleEnum;
+  const isPersonalView = viewerId === viewingId;
   // veteran personal profile view
-  if (viewerRole === VETERAN && viewingRole === VETERAN) {
+  if (isPersonalView && viewerRole === VETERAN && viewingRole === VETERAN) {
     context.showUserList = true;
     context.userListTitle = "Assigned Volunteers";
     context.viewingPersonalProfile = true;
     return context;
   }
   // volunteer personal profile view
-  else if (viewerRole === VOLUNTEER && viewingRole === VOLUNTEER) {
+  else if (isPersonalView && viewerRole === VOLUNTEER && viewingRole === VOLUNTEER) {
     context.showUserList = true;
     context.userListTitle = "Assigned Veterans";
     context.viewingPersonalProfile = true;
     return context;
   }
   // staff personal profile view
-  else if (viewerRole === STAFF && viewingRole === STAFF) {
+  else if (isPersonalView && viewerRole === STAFF && viewingRole === STAFF) {
     context.showUserList = true;
     context.userListTitle = "Veterans Under Point of Contact";
     context.viewingPersonalProfile = true;
     return context;
   }
   // admin personal profile view
-  else if (viewerRole === ADMIN && viewingRole === ADMIN) {
+  else if (isPersonalView && viewerRole === ADMIN && viewingRole === ADMIN) {
     context.viewingPersonalProfile = true;
     return context;
   }
@@ -111,11 +115,22 @@ function getProfileRenderingContext(
 }
 
 export default function UserProfile({ userId }: { userId: string }) {
+  /**
+   * NOTE: Because there is no authentication context at the moment,
+   * this component uses the following localStorage values to determine
+   * the logged in user's context:
+   *
+   * (1.) viewerId -> The id of the logged in user
+   * (2.) viewerRole -> The role of the logged in user
+   *
+   * These values can be set via the Chrome Developer Inspector
+   */
+
   const [userProfile, setUserProfile] = useState<UserProfileType | undefined>(undefined);
   const [profileRenderingContext, setProfileRenderingContext] = useState(
     getProfileRenderingContext(null, null),
   );
-  const viewerRole = localStorage.getItem("viewerRole") ?? "";
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchUserProfile = async () => {
@@ -127,73 +142,57 @@ export default function UserProfile({ userId }: { userId: string }) {
     fetchUserProfile()
       .then((res) => {
         setUserProfile(res);
+        setProfileRenderingContext(getProfileRenderingContext(res?.role, userId));
+        setLoading(false);
       })
       .catch((err: unknown) => {
         console.error(err);
+        setLoading(false);
       });
   }, []);
 
-  useEffect(() => {
-    setProfileRenderingContext(getProfileRenderingContext(viewerRole, userProfile?.role));
-  }, [userProfile]);
-
-  return (
-    <>
-      <div className={styles.viewerViewingSelector}>
-        <label htmlFor="viewerRole">Viewer Role</label>
-        <select
-          id="viewerRole"
-          value={viewerRole}
-          onChange={(event) => {
-            localStorage.setItem("viewerRole", event.target.value);
-            window.location.reload();
-          }}
-        >
-          <option value={RoleEnum.ADMIN}>Admin</option>
-          <option value={RoleEnum.STAFF}>Staff</option>
-          <option value={RoleEnum.VOLUNTEER}>Volunteer</option>
-          <option value={RoleEnum.VETERAN}>Veteran</option>
-        </select>
-      </div>
-      {profileRenderingContext?.invalidContext ? (
-        <h1>Error: Invalid Profile Rendering Context</h1>
-      ) : (
-        <div className={styles.userProfile}>
-          {profileRenderingContext?.viewingPersonalProfile ? (
-            <div className={styles.profileHeading}>Profile Information</div>
-          ) : (
-            <NavigateBack />
-          )}
-          <div className={styles.userProfileContent}>
-            <ProfileHeader
-              firstName={userProfile?.firstName}
-              lastName={userProfile?.lastName}
-              role={userProfile?.role}
-              assignedPrograms={userProfile?.assignedPrograms}
-              yearJoined={userProfile?.yearJoined}
-              age={userProfile?.age}
-              phoneNumber={userProfile?.phoneNumber}
-              gender={userProfile?.gender}
-              email={userProfile?.email}
-              isPersonalProfile={profileRenderingContext.viewingPersonalProfile}
-              isProgramAndRoleEditable={profileRenderingContext.isProgramAndRoleEditable}
-            />
-            <div className={styles.userProfileInnerContent}>
-              {profileRenderingContext.showVolunteerNotes && <VolunteerNotes userId={userId} />}
-              {profileRenderingContext.showUserList && (
-                <UserList
-                  userProfile={userProfile}
-                  title={profileRenderingContext.userListTitle}
-                  editable={profileRenderingContext.userListEditable}
-                  minimized={profileRenderingContext.showVolunteerNotes}
-                />
-              )}
+  if (!loading)
+    return (
+      <>
+        {profileRenderingContext?.invalidContext ? (
+          <h1>Error: Invalid Profile Rendering Context</h1>
+        ) : (
+          <div className={styles.userProfile}>
+            {profileRenderingContext?.viewingPersonalProfile ? (
+              <div className={styles.profileHeading}>Profile Information</div>
+            ) : (
+              <NavigateBack />
+            )}
+            <div className={styles.userProfileContent}>
+              <ProfileHeader
+                firstName={userProfile?.firstName}
+                lastName={userProfile?.lastName}
+                role={userProfile?.role}
+                assignedPrograms={userProfile?.assignedPrograms}
+                yearJoined={userProfile?.yearJoined}
+                age={userProfile?.age}
+                phoneNumber={userProfile?.phoneNumber}
+                gender={userProfile?.gender}
+                email={userProfile?.email}
+                isPersonalProfile={profileRenderingContext.viewingPersonalProfile}
+                isProgramAndRoleEditable={profileRenderingContext.isProgramAndRoleEditable}
+              />
+              <div className={styles.userProfileInnerContent}>
+                {profileRenderingContext.showVolunteerNotes && <VolunteerNotes userId={userId} />}
+                {profileRenderingContext.showUserList && (
+                  <UserList
+                    userProfile={userProfile}
+                    title={profileRenderingContext.userListTitle}
+                    editable={profileRenderingContext.userListEditable}
+                    minimized={profileRenderingContext.showVolunteerNotes}
+                  />
+                )}
+              </div>
             </div>
           </div>
-        </div>
-      )}
-    </>
-  );
+        )}
+      </>
+    );
 }
 
 function ProfilePicture(params: { firstName?: string; size?: string }) {
@@ -341,13 +340,14 @@ function VolunteerNotes({ userId }: { userId: string }) {
               event.preventDefault();
               const comment = {
                 profileId: userId,
-                commenterId: "67a4255fc7beaa03529393dc",
+                commenterId: localStorage.getItem("viewerId") ?? "67a4255fc7beaa03529393dc",
                 comment: currentComment,
                 datePosted: new Date(),
               };
               postProfileNote(comment)
                 .then(() => {
                   console.log("Comment posted");
+                  setCurrentComment("");
                 })
                 .catch((err: unknown) => {
                   console.error(err);
