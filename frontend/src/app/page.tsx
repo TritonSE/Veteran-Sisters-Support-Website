d47bc6b6b2f75bf6931bfad21ae03925f93d5d6f
Srikar Eranky <srikar.eranky@gmail.com>
2025-03-11 21:05:19 -0700

    profile auth context

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,40 +7,40 @@ import { useAuth } from "../contexts/AuthContext";
 
 import LoginForm from "./login/page";
 
-const getUserRole = async (email: string, attempts = 0): Promise<string> => {
-  try {
-    const response = await fetch(
-      `http://localhost:4000/api/users/role/${encodeURIComponent(email)}`,
-    );
+// const getUserRole = async (email: string, attempts = 0): Promise<string> => {
+//   try {
+//     const response = await fetch(
+//       `http://localhost:4000/api/users/role/${encodeURIComponent(email)}`,
+//     );
 
-    if (response.status === 404 && attempts < 3) {
-      console.warn("User record not found");
-      await new Promise((resolve) => setTimeout(resolve, 500));
-      return getUserRole(email, attempts + 1);
-    }
+//     if (response.status === 404 && attempts < 3) {
+//       console.warn("User record not found");
+//       await new Promise((resolve) => setTimeout(resolve, 500));
+//       return getUserRole(email, attempts + 1);
+//     }
 
-    if (!response.ok) {
-      throw new Error("Failed to fetch user role");
-    }
-    const data = await response.json();
-    console.log(data);
-    return data.role;
-  } catch (error) {
-    console.error("Error fetching user role:", error);
-    return "";
-  }
-};
+//     if (!response.ok) {
+//       throw new Error("Failed to fetch user role");
+//     }
+//     const data = await response.json();
+//     console.log(data);
+//     return data.role;
+//   } catch (error) {
+//     console.error("Error fetching user role:", error);
+//     return "";
+//   }
+// };
 
 export default function Home() {
-  const { loggedIn, currentUserId } = useAuth();
+  const { loggedIn, currentUserId, role } = useAuth();
   const router = useRouter();
-  const [role, setRole] = useState("");
+  // const [role, setRole] = useState("");
 
-  useEffect(() => {
-    if (loggedIn && currentUserId) {
-      getUserRole(currentUserId).then(setRole);
-    }
-  }, [loggedIn, currentUserId]);
+  // useEffect(() => {
+  //   if (loggedIn && currentUserId) {
+  //     getUserRole(currentUserId).then(setRole);
+  //   }
+  // }, [loggedIn, currentUserId]);
 
   useEffect(() => {
     if (loggedIn) {
--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -3,30 +3,38 @@ import React, { ReactNode, useContext, useEffect, useMemo, useState } from "reac
 
 import { auth } from "../../firebase/firebase";
 
-const getUserId = async (email: string, attempts = 0): Promise<string> => {
+type UserReturn = {
+  id: string;
+  role: string;
+};
+
+const getUserIdAndRole = async (email: string, attempts = 0): Promise<UserReturn> => {
   try {
-    const response = await fetch(`http://localhost:4000/api/users/id/${encodeURIComponent(email)}`);
+    const response = await fetch(
+      `http://localhost:4000/api/users/email/${encodeURIComponent(email)}`,
+    );
 
     if (response.status === 404 && attempts < 3) {
       console.warn("User record not found");
       await new Promise((resolve) => setTimeout(resolve, 500));
-      return getUserId(email, attempts + 1);
+      return getUserIdAndRole(email, attempts + 1);
     }
 
     if (!response.ok) {
       throw new Error("Failed to fetch user ID");
     }
     const data = await response.json();
-    return data.id;
+    return { id: data.id, role: data.role };
   } catch (error) {
     console.error("Error fetching user ID:", error);
-    return "";
+    return null;
   }
 };
 
 type contextType = {
   currentUser: User | undefined | null;
   currentUserId: string;
+  role: string;
   loggedIn: boolean;
   loading: boolean;
 };
@@ -40,6 +48,7 @@ export const AuthContext = React.createContext<contextType>({
   loggedIn: false,
   loading: false,
   currentUserId: "",
+  role: "",
 });
 
 export const useAuth = () => {
@@ -49,6 +58,7 @@ export const useAuth = () => {
 export function AuthProvider({ children }: ProviderProps) {
   const [currentUser, setCurrentUser] = useState<User | null>(null);
   const [currentUserId, setCurrentUserId] = useState(String);
+  const [role, setRole] = useState("");
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -56,11 +66,13 @@ export function AuthProvider({ children }: ProviderProps) {
       console.log("Auth state changed: ", user);
       if (user) {
         setCurrentUser(user);
-        const userId = await getUserId(user.email || "");
+        const { userId, role } = await getUserIdAndRole(user.email || "");
         setCurrentUserId(userId);
+        setRole(role);
       } else {
         setCurrentUser(null);
         setCurrentUserId("");
+        setRole("");
       }
       setLoading(false);
     });
@@ -69,8 +81,8 @@ export function AuthProvider({ children }: ProviderProps) {
   }, []);
 
   const value = useMemo(
-    () => ({ currentUser, loggedIn: !!currentUser, loading, currentUserId }),
-    [currentUser, loading],
+    () => ({ currentUser, role, loggedIn: !!currentUser, loading, currentUserId }),
+    [currentUser, role, loading],
   );
 
   if (loading) {
