8ac081ac9bf143b06f5d2cbab5fe081c827002d4
jliu0140 <122562868+jliu0140@users.noreply.github.com>
2025-06-05 16:01:15 -0700

    get and post announcement

--- backend/routes/activityRoutes.js
+++ backend/routes/activityRoutes.js
@@ -1,11 +1,31 @@
 import express from "express";
-import { getUnreadActivities, createActivity } from "../controllers/activityController.js";
+import {
+  getUnreadActivities,
+  createActivity,
+  createAnnouncement,
+  getAnnouncements,
+} from "../controllers/activityController.js";
 import { authenticateUser } from "../middleware/auth.js";
+import { authenticateStaffOrAdmin } from "../middleware/profile.js";
 
 const router = express.Router();
 
 router.get("/activities/:userId", authenticateUser, getUnreadActivities);
 
 router.post("/activities", authenticateUser, createActivity);
+
+router.post(
+  "/activities/announcement",
+  authenticateUser,
+  authenticateStaffOrAdmin,
+  createAnnouncement,
+);
+
+router.get(
+  "/activities/announcements/:userId",
+  authenticateUser,
+  authenticateStaffOrAdmin,
+  getAnnouncements,
+);
 
 export default router;
--- frontend/src/app/api/activities.ts
+++ frontend/src/app/api/activities.ts
@@ -1,4 +1,4 @@
-import { APIResult, get } from "./requests";
+import { APIResult, get, handleAPIError, post } from "./requests";
 
 export type ActivityObject = {
   _id: string;
@@ -11,6 +11,12 @@ export type ActivityObject = {
   isRead: boolean;
   createdAt: Date;
   relativeTime: string;
+};
+
+type CreateAnnouncementRequest = {
+  uploader: string;
+  title: string;
+  description: string;
 };
 
 // Set the relative time (Today OR # days ago OR Dec 12) based on timestamp
@@ -62,6 +68,37 @@ export const getUnreadActivities = async (
       const errorMessage = `Error: ${response.statusText}`;
       return { success: false, error: errorMessage };
     }
+  } catch (error: unknown) {
+    return { success: false, error: (error as Error).message };
+  }
+};
+
+export const getAnnouncements = async (userId: string): Promise<APIResult<ActivityObject[]>> => {
+  try {
+    const response = await get(`/activities/announcements/${userId}`);
+    if (!response.ok) {
+      return handleAPIError(response);
+    }
+    const announcements = (await response.json()) as ActivityObject[];
+    announcements.forEach((activity: ActivityObject) => {
+      activity.createdAt = new Date(activity.createdAt);
+    });
+    return { success: true, data: announcements };
+  } catch (error: unknown) {
+    return { success: false, error: (error as Error).message };
+  }
+};
+
+export const createAnnouncement = async (
+  announcementInfo: CreateAnnouncementRequest,
+): Promise<APIResult<ActivityObject>> => {
+  try {
+    const response = await post(`/activities/announcement`, announcementInfo);
+    if (!response.ok) {
+      return handleAPIError(response);
+    }
+    const activity = (await response.json()) as ActivityObject;
+    return { success: true, data: activity };
   } catch (error: unknown) {
     return { success: false, error: (error as Error).message };
   }
