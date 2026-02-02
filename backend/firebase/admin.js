efec91b93dce186bef63cb3b9dcba26fec9184fe
Eshaan Sharma <eshaansharma18@gmail.com>
2026-02-01 17:21:46 -0800

    fixed firebase/admin.js for deployment

--- backend/firebase/admin.js
+++ backend/firebase/admin.js
@@ -1,12 +1,30 @@
-import { initializeApp, cert } from "firebase-admin/app";
-import { getAuth } from "firebase-admin/auth";
-import dotenv from "dotenv";
+// UNCOMMENT IF USING LOCALLY
 
-dotenv.config();
+// import { initializeApp, cert } from "firebase-admin/app";
+// import { getAuth } from "firebase-admin/auth";
+// import dotenv from "dotenv";
 
-// Initialize Firebase Admin with service account
-const app = initializeApp({
-  credential: cert(JSON.parse(process.env.BACKEND_FIREBASE_SERVICE_ACCOUNT)),
-});
+// dotenv.config();
 
-export const auth = getAuth(app);
+// // Initialize Firebase Admin with service account
+// const app = initializeApp({
+//   credential: cert(JSON.parse(process.env.BACKEND_FIREBASE_SERVICE_ACCOUNT)),
+// });
+
+// export const auth = getAuth(app);
+
+// UNCOMMENT FOR DEPLOYED VERSION
+import { readFileSync } from "fs";
+
+import admin from "firebase-admin";
+
+if (!admin.apps.length) {
+  const raw = readFileSync("/etc/secrets/firebase-service-account.json", "utf8");
+  const serviceAccount = JSON.parse(raw);
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+
+export default admin;
